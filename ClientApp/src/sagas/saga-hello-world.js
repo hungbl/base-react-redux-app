0ef79027ddb3e0b5826cafe30e6019e55e93871f
Bui Ly Hung <hungbl6572@gmail.com>
2019-02-20 11:49:59 +0700

    fix saga bug

--- ClientApp/src/sagas/saga-hello-world.js
+++ ClientApp/src/sagas/saga-hello-world.js
@@ -5,13 +5,13 @@ import {helloWorld} from '../actions/action';
 
 export function* sagaHelloWorld(){
     while (true) {
-      const {payload} = yield take(c.SUBMIT_ED);
+      const {payload} = yield take(c.HELLO_WORLD);
       try {
         const url = 'hello-world/get-message';
   
         let res = yield call(axios.post, url, payload);
         if(res.data.status == "success"){
-          yield put(fetchOutput(res.data.data));
+          //yield put(fetchOutput(res.data.data));
         }else{
           alert('Something wrong happened!');
         }
